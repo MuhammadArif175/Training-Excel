00d215ac2100358ccaf10ba9b51ae590d1b7b829
Rif340 <arifgrup995@gmail.com>
2023-10-18 18:55:19 +0700

    Training Excel

---
+++ PT.Banshu Rubber Indonesia/online.main/Online-main/Online-main/halamanLogin.php
@@ -0,0 +1,51 @@
+<php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Patrick+Hand&family=Poppins&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+    </style>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+
+    <!-- Link Css -->
+    <link rel="halamanLogin.css" type="stylesheet">
+    <title>Halaman Login</title>
+</head>
+<body>
+    
+<div class="container bg-dark">
+    <div class="d-flex mt-4">
+    <div class="bg-primary">
+        <img src="https://github.com/MuhammadArif175/Gambar-Gambar/blob/main/gambar4.jpg?raw=true" style="width:200px;height:400px">
+    </div>
+    <div class="bg-danger d-grid">
+        <label> Masukin Email Loe</label><br>
+        <input placeholder="Masukan Email Anda" type="input" name="email" style="border-radius:20px">
+
+
+        <label>Masukan Pasword Loe</label><br>
+        <input type="Pasword" placeholder="Masukan Email Anda" name="pasword" style="border-radius:20px">
+    </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+</php>
